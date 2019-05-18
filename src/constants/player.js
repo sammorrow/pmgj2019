e8566312d8532142a03e445dac0f68f7631c8b53
Sam Morrow <smorrow@oberlin.edu>
2019-05-18 15:50:55 -0400

    collisions, movement, tilemap

---
+++ src/constants/player.js
@@ -0,0 +1,13 @@
+// basic movement
+export const FORWARD_VELOCITY = 200;
+export const BACKWARD_VELOCITY = -150;
+
+// lunging
+export const LUNGE_INITIAL_VELOCITY = 2000;
+
+export const LUNGE_DECELERATION = 1.25
+export const LUNGE_TIMEFRAME = 400;
+
+// cooldowns 
+export const MOVEMENT_COOLDOWN = 2000;
+export const COOLDOWN_TIC = 10;
---
+++ src/constants/world.js
@@ -0,0 +1,2 @@
+export const MOVEMENT_UPTIME = 50;
+export const MOVEMENT_DOWNTIME = 100;
