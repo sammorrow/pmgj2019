8c7455d5f20585a86f0d2f1e337f24395f324d8b
Dan Spector <danno2423@gmail.com>
2019-05-19 16:52:27 -0400

    added background to menu

--- src/states/Menu.js
+++ src/states/Menu.js
@@ -9,11 +9,13 @@ export default class extends Phaser.State {
     this.load.image('title', 'assets/images/menu/title.png');
     this.load.image('graphic', 'assets/images/menu/graphic.png');
     this.load.image('button', 'assets/images/menu/button.png');
+    this.load.image('tile', 'assets/images/menu/tilesprite.png')
     this.music = this.game.add.audio('loop2');
   }
 
   create() {
     this.music.play();
+    this.background = this.game.add.tileSprite(0, 0, 800, 600, 'tile')
     this.game.add.image(150, 50, 'title');
     this.game.add.image(300, 125, 'graphic').scale.setTo(.15, .15);
     this.button = this.game.add.image(325, 385, 'button');
@@ -25,6 +27,11 @@ export default class extends Phaser.State {
       this.state.start('Game');
       this.music.stop();
     }
+
+  } 
+
+    update() {
+      this.background.tilePosition.y -= 4;
+    }
   }
-}
 
