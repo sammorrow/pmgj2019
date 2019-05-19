f8b620322b4492ecd8d8cac8d5a2b0c2fca3f194
Dan Spector <danno2423@gmail.com>
2019-05-19 16:07:21 -0400

    Update Menu.js

--- src/states/Menu.js
+++ src/states/Menu.js
@@ -9,10 +9,11 @@ export default class extends Phaser.State {
     this.load.image('assets/images/menu/title.png');
     this.load.image('assets/images/menu/graphic.png');
     this.load.image('assets/images/menu/button.png');
+    
   }
 
   create() {
-
+    this.game.add.audio('loop2').play();
   }
 }
 
