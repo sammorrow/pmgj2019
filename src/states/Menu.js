3abcdecd60dfe3d9145cf07e972b8d73bf2a3007
Dan Spector <danno2423@gmail.com>
2019-05-19 16:36:46 -0400

    Update Menu.js

--- src/states/Menu.js
+++ src/states/Menu.js
@@ -13,15 +13,18 @@ export default class extends Phaser.State {
   }
 
   create() {
-    this.game.add.audio('loop2').play();
+    let music = this.game.add.audio('loop2');
+    music.play();
     this.game.add.image(150, 50, 'title');
     this.game.add.image(300, 125, 'graphic').scale.setTo(.15, .15);
-    this.button = this.game.add.image(325, 385, 'button').scale.setTo(.3, .3);
+    this.button = this.game.add.image(325, 385, 'button');
+    this.button.scale.setTo(.3, .3);
     this.button.inputEnabled = true;
     this.button.events.onInputDown.add(onClick, this);
 
     function onClick() {
       this.state.start('Game');
+      music.stop();
     }
   }
 }
